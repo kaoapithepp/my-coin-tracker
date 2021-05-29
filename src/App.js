a0bc6b4ea9a41adb10d85ab15386e0b23a0d41d2
kaoapithepp <simsonslt.ig@gmail.com>
2021-05-29 23:00:27 +0700

    Change NodeFetch to axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Coin from './components/Coin';
 import './App.css';
 import thumbnail from './data/thumbnail';
+
 
 const SHIB_FETCH_URL = 'https://api.coingecko.com/api/v3/coins/shiba-inu?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false';
 const ETH_FETCH_URL = 'https://api.coingecko.com/api/v3/coins/ethereum?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false';
@@ -19,79 +21,69 @@ function App() {
   const [shibName, setShibName] = useState('');
   const [shibPrice, setShibPrice] = useState(0);
 
+
   useEffect(() => {
     setInterval(() => {
-      fetch(ETH_FETCH_URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(resJson => {
-        setEthPrice(resJson.market_data.current_price.thb);
-        // return console.log('Price changed');
-      })
-      .catch(err => console.log(err.message));
+      axios
+        .get(ETH_FETCH_URL)
+        .then(resJson => {
+          // console.log(resJson);
+          setEthPrice(resJson.data.market_data.current_price.thb);
+        })
+        .catch(err => console.log(err.message));
     }, 1000);
   },[ethPrice]);
 
   useEffect(() => {
-    fetch(ETH_FETCH_URL)
-    .then(res => {
-      return res.json();
-    })
-    .then(resJson => {
-      setEthName(resJson.name);
-    })
-    .catch(err => console.log(err.message));
+    axios
+      .get(ETH_FETCH_URL)
+      .then(resJson => {
+        // console.log(resJson);
+        setEthName(resJson.data.name);
+      })
+      .catch(err => console.log(err.message));
   })
 
   useEffect(() => {
     setInterval(() => {
-      fetch(BNB_FETCH_URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(resJson => {
-        setBnbPrice(resJson.market_data.current_price.thb);
-        // return console.log('Price changed');
-      })
-      .catch(err => console.log(err.message));
+      axios
+        .get(BNB_FETCH_URL)
+        .then(resJson => {
+          setBnbPrice(resJson.data.market_data.current_price.thb);
+          // return console.log('Price changed');
+        })
+        .catch(err => console.log(err.message));
     }, 1000);
   },[bnbPrice]);
 
   useEffect(() => {
-    fetch(BNB_FETCH_URL)
-    .then(res => {
-      return res.json();
-    })
+    axios
+    .get(BNB_FETCH_URL)
     .then(resJson => {
-      setBnbName(resJson.name);
+      setBnbName(resJson.data.name);
     })
     .catch(err => console.log(err.message));
   })
 
   useEffect(() => {
     setInterval(() => {
-      fetch(SHIB_FETCH_URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(resJson => {
-        setShibPrice(resJson.market_data.current_price.thb);
-        // return console.log('Price changed');
-      })
-      .catch(err => console.log(err.message));
+      axios
+        .get(SHIB_FETCH_URL)
+        .then(resJson => {
+          setShibPrice(resJson.data.market_data.current_price.thb);
+          // return console.log('Price changed');
+        })
+        .catch(err => console.log(err.message));
     }, 1000);
   },[shibPrice]);
 
   useEffect(() => {
-    fetch(SHIB_FETCH_URL)
-    .then(res => {
-      return res.json();
-    })
-    .then(resJson => {
-      setShibName(resJson.name);
-    })
-    .catch(err => console.log(err.message));
+    axios
+      .get(SHIB_FETCH_URL)
+      .then(resJson => {
+        setShibName(resJson.data.name);
+      })
+      .catch(err => console.log(err.message));
   })
     
 
--- src/components/Coin.js
+++ src/components/Coin.js
@@ -5,7 +5,7 @@ const Coin = ({name, price, thumbnail}) => {
     return (
         <div className="coin-container">
             <div className="coin-display">
-                <img src={thumbnail}/>
+                <img src={thumbnail} alt="cryptocurrency logo" />
                 <h2>{name}</h2>
                 <p className="curr-price">Current Price</p>
                 <p>{price} THB</p>
